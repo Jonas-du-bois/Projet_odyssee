08c7fb4106cf1cf38af0adcc467b372d425db9c8
jonas-du-bois <jonas.du.bois@outlook.com>
2025-06-11 14:05:06 +0200

    Add manual Scribe documentation routes for Heroku deployment

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,3 +10,29 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return response()->json(['message' => 'Use API login endpoint'], 404);
 })->name('login');
+
+// Route manuelle pour la documentation Scribe (nécessaire sur Heroku)
+Route::get('/docs', function () {
+    return view('scribe.index');
+})->name('scribe');
+
+// Route pour servir les assets CSS/JS de Scribe
+Route::get('/docs/css/{file}', function ($file) {
+    $path = public_path('docs/css/' . $file);
+    if (file_exists($path)) {
+        return response()->file($path, [
+            'Content-Type' => 'text/css'
+        ]);
+    }
+    abort(404);
+});
+
+Route::get('/docs/js/{file}', function ($file) {
+    $path = public_path('docs/js/' . $file);
+    if (file_exists($path)) {
+        return response()->file($path, [
+            'Content-Type' => 'application/javascript'
+        ]);
+    }
+    abort(404);
+});
